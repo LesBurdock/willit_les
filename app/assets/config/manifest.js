e83ac7bf9841bf5bf07011a71d1287855fbd31b2
Lesley Burdock <lesleyburdock@Lesleys-MacBook-Air.local>
2023-06-29 11:21:59 +0200

    tryiong to update

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,7 @@
 //= link_tree ../images
-
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .json
 
 
-// = link application.css
+// = link application.scss
